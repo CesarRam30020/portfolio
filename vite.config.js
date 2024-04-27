2fa3d89f67395175886dedcb1446770f94baa74e
CesarRam <Cesar.Ram.30020@gmail.com>
2024-04-26 20:08:32 -0600

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import vue from '.src/vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),],
   base: 'https://CesarRam30020.github.io/portfolio',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
